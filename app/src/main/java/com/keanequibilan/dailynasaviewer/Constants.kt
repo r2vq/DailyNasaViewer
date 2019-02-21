c824e15584c59db4a69946e1f690784721a62b08
Keane Quibilan <arv.r2vq@gmail.com>
2019-02-21 14:59:31 -0500

    Create constants for NASA APOD endpoint.

---
+++ app/src/main/java/com/keanequibilan/dailynasaviewer/Constants.kt
@@ -0,0 +1,18 @@
+package com.keanequibilan.dailynasaviewer
+
+const val NASA_API_BASE_URL = "https://api.nasa.gov/"
+const val NASA_API_KEY = "NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo"
+const val NASA_API_PATH_APOD = "planetary/apod"
+
+const val APOD_QUERY_PARAM_DATE = "date"
+const val APOD_QUERY_PARAM_HD = "hd"
+const val APOD_QUERY_PARAM_API_KEY = "api_key"
+
+const val APOD_FIELD_COPYRIGHT = "copyright"
+const val APOD_FIELD_DATE = "date"
+const val APOD_FIELD_EXPLANATION = "explanation"
+const val APOD_FIELD_HD_URL = "hdurl"
+const val APOD_FIELD_MEDIA_TYPE = "media_type"
+const val APOD_FIELD_SERVICE_VERSION = "service_version"
+const val APOD_FIELD_TITLE = "title"
+const val APOD_FIELD_URL = "url"
